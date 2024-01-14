042c6e1407b2eb1898db97a425d69141a824869b
FM-Akib <muntasirakib08@gmail.com>
2024-01-14 20:11:12 +0600

    done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="Footer">
+            All rights reserved - Akib.
+        </div>
+    );
+};
+
+export default Footer;
